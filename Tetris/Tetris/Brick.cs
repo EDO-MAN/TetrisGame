ed121258639430310af450b0754a14823166b3db
EDO-MAN <73830965+EDO-MAN@users.noreply.github.com>
2021-04-17 02:51:54 +0900

    테트리스 제작 중
    블럭 이동 설정.
    게임 틀 설정.

---
+++ Tetris/Tetris/Brick.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Brick
+    {
+        internal int X
+        {
+            get;
+            private set;
+        }
+        internal int Y
+        {
+            get;
+            private set;
+        }
+        internal Brick()
+        {
+            Reset();
+        }
+        internal void Reset()
+        {
+            X = GameRule.Start_X;
+            Y = GameRule.Start_Y;
+        }
+        internal void MoveLeft()
+        {
+            X--;
+        }
+        internal void MoveRight()
+        {
+            X++;
+        }
+        internal void MoveDown()
+        {
+            Y++;
+        }
+    }
+}
---
+++ Tetris/Tetris/GameRule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    static class GameRule
+    {
+        internal const int Board_X = 10;//게임판 가로길이
+        internal const int Board_Y = 20;//게임판 세로길이
+
+        internal const int Start_X = 5;//블럭 시작 좌표X
+        internal const int Start_Y = 0;//블럭 시작 좌표Y
+
+        internal const int Pixel_X = 20;//게임 1칸당 X값
+        internal const int Pixel_Y = 20;//게임 1칸당 치Y
+    }
+}
