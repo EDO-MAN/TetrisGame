2b0aa3285b99e3dbf2788dd81d88add2db36cb0b
EDO-MAN <73830965+EDO-MAN@users.noreply.github.com>
2021-04-17 20:50:17 +0900

    어렵구먼

--- Tetris/Tetris/Form1.Designer.cs
+++ Tetris/Tetris/Form1.Designer.cs
@@ -28,23 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(58, 55);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 400);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(506, 575);
-            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -52,8 +42,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
     }
 }
 
